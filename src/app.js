ee4097ef9d2d20ca5bfddc3c6f9058307e8f9785
shankarbr <shankarbr.8991@gmail.com>
2024-11-12 19:09:40 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@ const express = require('express');
 
 const app = express(); //Creating a new express js application
 
+// The result be HAHAHA irrepspective of the method because  of the order 
+app.use("/user", (req, res) => {
+    res.send("HAHAHAHAHA")
+});
+
+
+//This will handle only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstname: "Shankar", Lastname: "BR"})
+});
+
+app.post("/user", (req, res) => {
+    // Save data to DB
+    res.send("Data Successfully saved to data base")
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Delted Succesfully")
+});
+
+// Thi will match all the http methpod API calls to /test
 app.use("/test",(req,res) => {                         //Request Handler
-    res.send("Hello from the serverrrrrr!")
+    res.send("Hello from the serverrrrrrr!")
 });
 
 app.listen(3000, ()=> {
     console.log("Server is successfully listening on port 3000.....")
 });
+
+//Order of the code is VV Important/ matters a lot
