be95f05beb8bf33d40ae061cfd4eff60ff7c403c
shankarbr <shankarbr.8991@gmail.com>
2024-11-13 20:25:09 +0530

    write-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,30 +2,66 @@ const express = require('express');
 
 const app = express(); //Creating a new express js application
 
-// The result be HAHAHA irrepspective of the method because  of the order 
-app.use("/user", (req, res) => {
-    res.send("HAHAHAHAHA")
-});
+// The result be HAHAHA irrepspective of the method because  of the order if we use app.use in the starting as it matches with all methods.
 
 
 //This will handle only handle GET call to /user
-app.get("/user", (req, res) => {
-    res.send({firstname: "Shankar", Lastname: "BR"})
+
+// req /user, /user/xyz, /user/1
+
+// here b is optional coz o f ?. /ac /abc, using + => /abc, /abbbbc, ab*cd => abANYTHINGcd, a(bc)?cd => bc is optional
+// // app.get(/a/) => here /car will work since ot contains a. (These are called Rejex), /.*fly$/ => ANYTHINGfly
+
+// app.get("/user", (req, res) => {
+//     console.log(req.query); // to get control on the userID // this is how we read the query parameters
+//     res.send({firstname: "Shankar", Lastname: "BR"})
+// });
+
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//     console.log(req.params); // To handle the dynamic routing
+//     res.send({firstname: "Shankar", Lastname: "BR"})
+// });
+// app.use(
+//     "/user", [
+// (req, res, next) => { // This function is known as route handlers.
+//     console.log("Handling the route user 1")
+//     // res.send("route handler 1"); // if there is no res.send it will just send request with infinite loop.
+//     next(); 
+// },
+// (req, res, next) => {
+//     console.log("Handling the route user 2")
+//     // res.send("route handler 2");
+//     next();
+// },
+// (req, res, next) => {
+//     console.log("Handling the route user 3")
+//     res.send("route handler 3");
+//     // next(); Gives an error in the Postman.
+// }
+// ]);
+
+const  {adminAuth, userAuth } = require("./middlewares/auth")
+//Handle middleware for all GET, POST, ..requests.
+app.use("/admin", adminAuth);
+app.use("/user", userAuth);
+
+
+app.get("/user", (req, res) =>{
+    res.send("User data sent")
+    });
+
+
+app.get("/admin/getAlldata", (req, res) =>{
+//Logic of fetching all data
+// Logic of Checking if the request is authenticated
+res.send("All data sent")
 });
 
-app.post("/user", (req, res) => {
-    // Save data to DB
-    res.send("Data Successfully saved to data base")
-});
+app.get("/admin/deleteUser", (req, res) =>{
+    //Logic of fetching all data
+    res.send("Deleted the User")
+ });
 
-app.delete("/user", (req, res) => {
-    res.send("Delted Succesfully")
-});
-
-// Thi will match all the http methpod API calls to /test
-app.use("/test",(req,res) => {                         //Request Handler
-    res.send("Hello from the serverrrrrrr!")
-});
 
 app.listen(3000, ()=> {
     console.log("Server is successfully listening on port 3000.....")
